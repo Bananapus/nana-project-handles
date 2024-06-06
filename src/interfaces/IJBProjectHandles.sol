5a876e1575f318f96dafedca230cfcc118b1a160
mejango <mejango@protonmail.com>
2024-06-06 14:52:13 -0300

    fmt

--- src/interfaces/IJBProjectHandles.sol
+++ src/interfaces/IJBProjectHandles.sol
@@ -5,24 +5,18 @@ import "@ensdomains/ens-contracts/contracts/resolvers/profiles/ITextResolver.sol
 import "@bananapus/core/src/interfaces/IJBProjects.sol";
 
 interface IJBProjectHandles {
-    event SetEnsNameParts(
-        uint256 indexed projectId,
-        string indexed handle,
-        string[] parts,
-        address caller
-    );
+    event SetEnsNameParts(uint256 indexed projectId, string indexed handle, string[] parts, address caller);
 
-    function setEnsNamePartsFor(
-        uint256 chainId,
-        uint256 projectId,
-        string[] memory parts
-    ) external;
+    function setEnsNamePartsFor(uint256 chainId, uint256 projectId, string[] memory parts) external;
 
     function ensNamePartsOf(
         uint256 chainId,
         uint256 projectId,
         address projectOwner
-    ) external view returns (string[] memory);
+    )
+        external
+        view
+        returns (string[] memory);
 
     function TEXT_KEY_PREFIX() external view returns (string memory);
 
@@ -32,9 +26,5 @@ interface IJBProjectHandles {
 
     function PROJECTS() external view returns (IJBProjects);
 
-    function handleOf(
-        uint256 chainId,
-        uint256 projectId,
-        address projectOwner
-    ) external view returns (string memory);
+    function handleOf(uint256 chainId, uint256 projectId, address projectOwner) external view returns (string memory);
 }
